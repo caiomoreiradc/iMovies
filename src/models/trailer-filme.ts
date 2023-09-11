7574693bdb142c841f510a1b15dcd3087ed6c978
caiomoreiradc <caiomoreiracarvalhoo@outlook.com>
2023-09-11 01:52:04 -0300

    Start

---
+++ src/models/trailer-filme.ts
@@ -0,0 +1,15 @@
+export class TrailerFilme
+{
+    id: number;
+    sourceUrl: string;
+  
+    constructor(
+      id: number,
+      sourceUrl: string
+  
+    )
+    {
+      this.id = id;
+      this.sourceUrl = sourceUrl;
+    }
+  }
--- src/views/filme-listagem/filme-listagem.ts
+++ src/views/filme-listagem/filme-listagem.ts
@@ -1,18 +1,185 @@
 import "bootstrap";
-
 import "./filme-listagem.css";
+import { API_KEY } from "../../../secret";
+import { v4 as uuidv4 } from 'uuid';
+import { ListagemFilme } from "../../models/listagem-filme";
+import { FilmeService } from "../../services/filmes.services";
 
-function obterFilmesApi() {
-  fetch("https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1", {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTAzMzM1YjlmMTAxYjQzODNhOWQ4ZjAxNmRiNGM1ZiIsInN1YiI6IjY0YWVlNjliNjZhMGQzMDBjNjcwZjdlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.maqe-EQttCKGgwuXNLSalnq-BseNByKVa64rRPW8EGI",
-    },
-  })
-    .then((res) => res.json())
-    .then((obj) => console.log(obj));
+console.log(uuidv4());
+
+export class VisualizarListagemFilmes {
+  FilmeService: FilmeService;
+  pnlPrincipal: HTMLDivElement;
+
+  constructor() {
+    this.FilmeService = new FilmeService();
+    this.registrarElementos();
+    this.FilmeService.selecionarFilmePorPopularidade()
+    .then(filmes => this.filtrarPorPopularidade(filmes));
+    this.FilmeService.selecionarFilmeLancamento()
+    .then(filmes => this.exibirLancamentos(filmes));
+    this.FilmeService.selecionarFilmeEmBreve()
+    .then(filmes => this.exibirEmBreve(filmes));
+  }
+
+  registrarElementos(){
+    this.pnlPrincipal = document.getElementById("pnlPrincipal") as HTMLDivElement;
+  }
+
+
+  private filtrarPorPopularidade(filmes: ListagemFilme[]): void{
+    const lblTitulo = document.createElement("h2");
+    lblTitulo.textContent = "Filmes Em Alta";
+    lblTitulo.classList.add("fs-1", "text-warning");
+
+    const linhaCards = document.createElement("div");
+    linhaCards.classList.add("row", "g-3");
+
+    linhaCards.appendChild(lblTitulo);
+
+    for(let filme of filmes){
+      const coluna = document.createElement("div");
+
+      coluna.classList.add(
+        "app-coluna-poster",
+        "col-6",
+        "col-md-4",
+        "col-lg-2"
+        );
+
+        coluna.classList.add("text-center");
+
+        coluna.addEventListener("click", () => {
+          window.location.href = filme.urlDetalhes;
+        });
+
+        const card = document.createElement("div");
+        card.classList.add("d-grid", "gap-2");
+
+        const imgFilme = document.createElement("img");
+        imgFilme.classList.add("img-fluid", "rounded-3");
+        imgFilme.src = filme.urlPoster;
+
+        const lblTituloFilme = document.createElement("a");
+        lblTituloFilme.classList.add(
+          "fs-5",
+          "link-warning",
+          "text-decoration-none"
+        );
+        lblTituloFilme.textContent = filme.titulo;
+        lblTituloFilme.href = filme.urlDetalhes;
+
+        card.appendChild(imgFilme);
+        card.appendChild(lblTituloFilme);
+        coluna.appendChild(card);
+        linhaCards.appendChild(coluna);
+    }
+
+    this.pnlPrincipal.appendChild(linhaCards);
+  }
+
+  private exibirLancamentos(filmes: ListagemFilme[]): void{
+    const lblTitulo = document.createElement("h2");
+    lblTitulo.textContent = "Lançamentos";
+    lblTitulo.classList.add("fs-1", "text-warning");
+
+    const linhaCards = document.createElement("div");
+    linhaCards.classList.add("row", "g-3");
+
+    linhaCards.appendChild(lblTitulo);
+
+    for(let filme of filmes){
+      const coluna = document.createElement("div");
+
+      coluna.classList.add(
+        "app-coluna-poster",
+        "col-6",
+        "col-md-4",
+        "col-lg-2"
+        );
+
+        coluna.classList.add("text-center");
+
+        coluna.addEventListener("click", () => {
+          window.location.href = filme.urlDetalhes;
+        });
+
+        const card = document.createElement("div");
+        card.classList.add("d-grid", "gap-2");
+
+        const imgFilme = document.createElement("img");
+        imgFilme.classList.add("img-fluid", "rounded-3");
+        imgFilme.src = filme.urlPoster;
+
+        const lblTituloFilme = document.createElement("a");
+        lblTituloFilme.classList.add(
+          "fs-5",
+          "link-warning",
+          "text-decoration-none"
+        );
+        lblTituloFilme.textContent = filme.titulo;
+        lblTituloFilme.href = filme.urlDetalhes;
+
+        card.appendChild(imgFilme);
+        card.appendChild(lblTituloFilme);
+        coluna.appendChild(card);
+        linhaCards.appendChild(coluna);
+    }
+
+    this.pnlPrincipal.appendChild(linhaCards);
+  }
+
+  private exibirEmBreve(filmes: ListagemFilme[]): void{
+    const lblTitulo = document.createElement("h2");
+    lblTitulo.textContent = "Em Breve";
+    lblTitulo.classList.add("fs-1", "text-warning");
+
+    const linhaCards = document.createElement("div");
+    linhaCards.classList.add("row", "g-3");
+
+    linhaCards.appendChild(lblTitulo);
+
+    for(let filme of filmes){
+      const coluna = document.createElement("div");
+
+      coluna.classList.add(
+        "app-coluna-poster",
+        "col-6",
+        "col-md-4",
+        "col-lg-2"
+        );
+
+        coluna.classList.add("text-center");
+
+        coluna.addEventListener("click", () => {
+          window.location.href = filme.urlDetalhes;
+        });
+
+        const card = document.createElement("div");
+        card.classList.add("d-grid", "gap-2");
+
+        const imgFilme = document.createElement("img");
+        imgFilme.classList.add("img-fluid", "rounded-3");
+        imgFilme.src = filme.urlPoster;
+
+        const lblTituloFilme = document.createElement("a");
+        lblTituloFilme.classList.add(
+          "fs-5",
+          "link-warning",
+          "text-decoration-none"
+        );
+        lblTituloFilme.textContent = filme.titulo;
+        lblTituloFilme.href = filme.urlDetalhes;
+
+        card.appendChild(imgFilme);
+        card.appendChild(lblTituloFilme);
+        coluna.appendChild(card);
+        linhaCards.appendChild(coluna);
+    }
+
+    this.pnlPrincipal.appendChild(linhaCards);
+  }
 }
 
-obterFilmesApi();
+
+window.addEventListener('load', () => new VisualizarListagemFilmes());
